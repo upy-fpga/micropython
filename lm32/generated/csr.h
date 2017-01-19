8a48b7d2147c23bfa8725cd9ba06fd81df72af22
Katie Bell <katie@katharos.id.au>
2017-01-19 22:06:45 +1100

    lm32: Update generated headers
    Updated to use mimasv2 using headers from HDMI2USB build at commit
    cbd1e31b3a9f88e0d1ecb682b51f6d41454766de

--- lm32/generated/csr.h
+++ lm32/generated/csr.h
@@ -1,12 +1,12 @@
 #ifndef __GENERATED_CSR_H
 #define __GENERATED_CSR_H
-#include "hw/common.h"
+#include <hw/common.h>
 
-/* dna */
-#define CSR_DNA_BASE 0xe0006000
-#define CSR_DNA_ID_ADDR 0xe0006000
-#define CSR_DNA_ID_SIZE 8
-static inline unsigned long long int dna_id_read(void) {
+/* info */
+#define CSR_INFO_BASE 0xe0006000
+#define CSR_INFO_DNA_ID_ADDR 0xe0006000
+#define CSR_INFO_DNA_ID_SIZE 8
+static inline unsigned long long int info_dna_id_read(void) {
 	unsigned long long int r = MMPTR(0xe0006000);
 	r <<= 8;
 	r |= MMPTR(0xe0006004);
@@ -22,6 +22,48 @@ static inline unsigned long long int dna_id_read(void) {
 	r |= MMPTR(0xe0006018);
 	r <<= 8;
 	r |= MMPTR(0xe000601c);
+	return r;
+}
+#define CSR_INFO_GIT_COMMIT_ADDR 0xe0006020
+#define CSR_INFO_GIT_COMMIT_SIZE 20
+#define CSR_INFO_PLATFORM_PLATFORM_ADDR 0xe0006070
+#define CSR_INFO_PLATFORM_PLATFORM_SIZE 8
+static inline unsigned long long int info_platform_platform_read(void) {
+	unsigned long long int r = MMPTR(0xe0006070);
+	r <<= 8;
+	r |= MMPTR(0xe0006074);
+	r <<= 8;
+	r |= MMPTR(0xe0006078);
+	r <<= 8;
+	r |= MMPTR(0xe000607c);
+	r <<= 8;
+	r |= MMPTR(0xe0006080);
+	r <<= 8;
+	r |= MMPTR(0xe0006084);
+	r <<= 8;
+	r |= MMPTR(0xe0006088);
+	r <<= 8;
+	r |= MMPTR(0xe000608c);
+	return r;
+}
+#define CSR_INFO_PLATFORM_TARGET_ADDR 0xe0006090
+#define CSR_INFO_PLATFORM_TARGET_SIZE 8
+static inline unsigned long long int info_platform_target_read(void) {
+	unsigned long long int r = MMPTR(0xe0006090);
+	r <<= 8;
+	r |= MMPTR(0xe0006094);
+	r <<= 8;
+	r |= MMPTR(0xe0006098);
+	r <<= 8;
+	r |= MMPTR(0xe000609c);
+	r <<= 8;
+	r |= MMPTR(0xe00060a0);
+	r <<= 8;
+	r |= MMPTR(0xe00060a4);
+	r <<= 8;
+	r |= MMPTR(0xe00060a8);
+	r <<= 8;
+	r |= MMPTR(0xe00060ac);
 	return r;
 }
 
@@ -76,51 +118,138 @@ static inline void sdram_dfii_pi0_baddress_write(unsigned char value) {
 	MMPTR(0xe0004014) = value;
 }
 #define CSR_SDRAM_DFII_PI0_WRDATA_ADDR 0xe0004018
-#define CSR_SDRAM_DFII_PI0_WRDATA_SIZE 2
-static inline unsigned short int sdram_dfii_pi0_wrdata_read(void) {
-	unsigned short int r = MMPTR(0xe0004018);
+#define CSR_SDRAM_DFII_PI0_WRDATA_SIZE 4
+static inline unsigned int sdram_dfii_pi0_wrdata_read(void) {
+	unsigned int r = MMPTR(0xe0004018);
 	r <<= 8;
 	r |= MMPTR(0xe000401c);
-	return r;
-}
-static inline void sdram_dfii_pi0_wrdata_write(unsigned short int value) {
-	MMPTR(0xe0004018) = value >> 8;
-	MMPTR(0xe000401c) = value;
-}
-#define CSR_SDRAM_DFII_PI0_RDDATA_ADDR 0xe0004020
-#define CSR_SDRAM_DFII_PI0_RDDATA_SIZE 2
-static inline unsigned short int sdram_dfii_pi0_rddata_read(void) {
-	unsigned short int r = MMPTR(0xe0004020);
+	r <<= 8;
+	r |= MMPTR(0xe0004020);
 	r <<= 8;
 	r |= MMPTR(0xe0004024);
 	return r;
 }
-
-/* spiflash */
-#define CSR_SPIFLASH_BASE 0xe0005000
-#define CSR_SPIFLASH_BITBANG_ADDR 0xe0005000
-#define CSR_SPIFLASH_BITBANG_SIZE 1
-static inline unsigned char spiflash_bitbang_read(void) {
-	unsigned char r = MMPTR(0xe0005000);
+static inline void sdram_dfii_pi0_wrdata_write(unsigned int value) {
+	MMPTR(0xe0004018) = value >> 24;
+	MMPTR(0xe000401c) = value >> 16;
+	MMPTR(0xe0004020) = value >> 8;
+	MMPTR(0xe0004024) = value;
+}
+#define CSR_SDRAM_DFII_PI0_RDDATA_ADDR 0xe0004028
+#define CSR_SDRAM_DFII_PI0_RDDATA_SIZE 4
+static inline unsigned int sdram_dfii_pi0_rddata_read(void) {
+	unsigned int r = MMPTR(0xe0004028);
+	r <<= 8;
+	r |= MMPTR(0xe000402c);
+	r <<= 8;
+	r |= MMPTR(0xe0004030);
+	r <<= 8;
+	r |= MMPTR(0xe0004034);
 	return r;
 }
-static inline void spiflash_bitbang_write(unsigned char value) {
-	MMPTR(0xe0005000) = value;
-}
-#define CSR_SPIFLASH_MISO_ADDR 0xe0005004
-#define CSR_SPIFLASH_MISO_SIZE 1
-static inline unsigned char spiflash_miso_read(void) {
-	unsigned char r = MMPTR(0xe0005004);
+#define CSR_SDRAM_DFII_PI1_COMMAND_ADDR 0xe0004038
+#define CSR_SDRAM_DFII_PI1_COMMAND_SIZE 1
+static inline unsigned char sdram_dfii_pi1_command_read(void) {
+	unsigned char r = MMPTR(0xe0004038);
 	return r;
 }
-#define CSR_SPIFLASH_BITBANG_EN_ADDR 0xe0005008
-#define CSR_SPIFLASH_BITBANG_EN_SIZE 1
-static inline unsigned char spiflash_bitbang_en_read(void) {
-	unsigned char r = MMPTR(0xe0005008);
+static inline void sdram_dfii_pi1_command_write(unsigned char value) {
+	MMPTR(0xe0004038) = value;
+}
+#define CSR_SDRAM_DFII_PI1_COMMAND_ISSUE_ADDR 0xe000403c
+#define CSR_SDRAM_DFII_PI1_COMMAND_ISSUE_SIZE 1
+static inline unsigned char sdram_dfii_pi1_command_issue_read(void) {
+	unsigned char r = MMPTR(0xe000403c);
 	return r;
 }
-static inline void spiflash_bitbang_en_write(unsigned char value) {
-	MMPTR(0xe0005008) = value;
+static inline void sdram_dfii_pi1_command_issue_write(unsigned char value) {
+	MMPTR(0xe000403c) = value;
+}
+#define CSR_SDRAM_DFII_PI1_ADDRESS_ADDR 0xe0004040
+#define CSR_SDRAM_DFII_PI1_ADDRESS_SIZE 2
+static inline unsigned short int sdram_dfii_pi1_address_read(void) {
+	unsigned short int r = MMPTR(0xe0004040);
+	r <<= 8;
+	r |= MMPTR(0xe0004044);
+	return r;
+}
+static inline void sdram_dfii_pi1_address_write(unsigned short int value) {
+	MMPTR(0xe0004040) = value >> 8;
+	MMPTR(0xe0004044) = value;
+}
+#define CSR_SDRAM_DFII_PI1_BADDRESS_ADDR 0xe0004048
+#define CSR_SDRAM_DFII_PI1_BADDRESS_SIZE 1
+static inline unsigned char sdram_dfii_pi1_baddress_read(void) {
+	unsigned char r = MMPTR(0xe0004048);
+	return r;
+}
+static inline void sdram_dfii_pi1_baddress_write(unsigned char value) {
+	MMPTR(0xe0004048) = value;
+}
+#define CSR_SDRAM_DFII_PI1_WRDATA_ADDR 0xe000404c
+#define CSR_SDRAM_DFII_PI1_WRDATA_SIZE 4
+static inline unsigned int sdram_dfii_pi1_wrdata_read(void) {
+	unsigned int r = MMPTR(0xe000404c);
+	r <<= 8;
+	r |= MMPTR(0xe0004050);
+	r <<= 8;
+	r |= MMPTR(0xe0004054);
+	r <<= 8;
+	r |= MMPTR(0xe0004058);
+	return r;
+}
+static inline void sdram_dfii_pi1_wrdata_write(unsigned int value) {
+	MMPTR(0xe000404c) = value >> 24;
+	MMPTR(0xe0004050) = value >> 16;
+	MMPTR(0xe0004054) = value >> 8;
+	MMPTR(0xe0004058) = value;
+}
+#define CSR_SDRAM_DFII_PI1_RDDATA_ADDR 0xe000405c
+#define CSR_SDRAM_DFII_PI1_RDDATA_SIZE 4
+static inline unsigned int sdram_dfii_pi1_rddata_read(void) {
+	unsigned int r = MMPTR(0xe000405c);
+	r <<= 8;
+	r |= MMPTR(0xe0004060);
+	r <<= 8;
+	r |= MMPTR(0xe0004064);
+	r <<= 8;
+	r |= MMPTR(0xe0004068);
+	return r;
+}
+#define CSR_SDRAM_CONTROLLER_BANDWIDTH_UPDATE_ADDR 0xe000406c
+#define CSR_SDRAM_CONTROLLER_BANDWIDTH_UPDATE_SIZE 1
+static inline unsigned char sdram_controller_bandwidth_update_read(void) {
+	unsigned char r = MMPTR(0xe000406c);
+	return r;
+}
+static inline void sdram_controller_bandwidth_update_write(unsigned char value) {
+	MMPTR(0xe000406c) = value;
+}
+#define CSR_SDRAM_CONTROLLER_BANDWIDTH_NREADS_ADDR 0xe0004070
+#define CSR_SDRAM_CONTROLLER_BANDWIDTH_NREADS_SIZE 3
+static inline unsigned int sdram_controller_bandwidth_nreads_read(void) {
+	unsigned int r = MMPTR(0xe0004070);
+	r <<= 8;
+	r |= MMPTR(0xe0004074);
+	r <<= 8;
+	r |= MMPTR(0xe0004078);
+	return r;
+}
+#define CSR_SDRAM_CONTROLLER_BANDWIDTH_NWRITES_ADDR 0xe000407c
+#define CSR_SDRAM_CONTROLLER_BANDWIDTH_NWRITES_SIZE 3
+static inline unsigned int sdram_controller_bandwidth_nwrites_read(void) {
+	unsigned int r = MMPTR(0xe000407c);
+	r <<= 8;
+	r |= MMPTR(0xe0004080);
+	r <<= 8;
+	r |= MMPTR(0xe0004084);
+	return r;
+}
+#define CSR_SDRAM_CONTROLLER_BANDWIDTH_DATA_WIDTH_ADDR 0xe0004088
+#define CSR_SDRAM_CONTROLLER_BANDWIDTH_DATA_WIDTH_SIZE 1
+static inline unsigned char sdram_controller_bandwidth_data_width_read(void) {
+	unsigned char r = MMPTR(0xe0004088);
+	return r;
 }
 
 /* timer0 */
@@ -293,10 +422,44 @@ static inline void uart_phy_tuning_word_write(unsigned int value) {
 
 /* constants */
 #define UART_INTERRUPT 0
+static inline int uart_interrupt_read(void) {
+	return 0;
+}
 #define TIMER0_INTERRUPT 1
-#define SYSTEM_CLOCK_FREQUENCY 80000000
+static inline int timer0_interrupt_read(void) {
+	return 1;
+}
+#define CSR_DATA_WIDTH 8
+static inline int csr_data_width_read(void) {
+	return 8;
+}
+#define SYSTEM_CLOCK_FREQUENCY 83333333
+static inline int system_clock_frequency_read(void) {
+	return 83333333;
+}
 #define SPIFLASH_PAGE_SIZE 256
+static inline int spiflash_page_size_read(void) {
+	return 256;
+}
 #define SPIFLASH_SECTOR_SIZE 65536
+static inline int spiflash_sector_size_read(void) {
+	return 65536;
+}
 #define L2_SIZE 8192
+static inline int l2_size_read(void) {
+	return 8192;
+}
+#define CONFIG_CLOCK_FREQUENCY 83333333
+static inline int config_clock_frequency_read(void) {
+	return 83333333;
+}
+#define CONFIG_CPU_TYPE "LM32"
+static inline const char * config_cpu_type_read(void) {
+	return "LM32";
+}
+#define CONFIG_CSR_DATA_WIDTH 8
+static inline int config_csr_data_width_read(void) {
+	return 8;
+}
 
 #endif
