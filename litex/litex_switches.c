1beea01b70c9965425622e5a9632c755379e7115
Karl Malakoff <karltheawesome@gmail.com>
2017-08-11 18:47:15 +1000

    Various fixes for litex cas module
    * Added switch class
    * All classes now named using pep8 standard
    * All classes can now use generic number of devices and have class
    constant to tell you how many there are
    
    TODO: implement and test IRQ for buttons

--- litex/litex_switches.c
+++ litex/litex_switches.c
@@ -9,6 +9,7 @@
 #ifndef CSR_CAS_BASE
 static inline void cas_switches_in_read(void) {
 }
+#define CAS_NUM_SWITCHES 0 
 #endif
 
 const mp_obj_type_t litex_switch_type;
@@ -18,39 +19,30 @@ typedef struct _litex_switch_obj_t {
     int num;
 } litex_switch_obj_t;
 
-STATIC litex_switch_obj_t litex_switches[8] = {
-	{{&litex_switch_type}, 1},
-	{{&litex_switch_type}, 2},
-	{{&litex_switch_type}, 3},
-	{{&litex_switch_type}, 4},
-	{{&litex_switch_type}, 5},
-	{{&litex_switch_type}, 6},
-	{{&litex_switch_type}, 7},
-	{{&litex_switch_type}, 8}
-};
-
 STATIC mp_obj_t litex_switch_make_new(const mp_obj_type_t *type_in,
 		size_t n_args, size_t n_kw, const mp_obj_t *args) {
 	mp_arg_check_num(n_args, n_kw, 1, 1, false);
 
 	mp_uint_t switch_num = mp_obj_get_int(args[0]);
 
-	switch (switch_num) {
-	case 1 ... 8:
-		return &litex_switches[switch_num - 1];
-	default:
-		nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_ValueError,
+    if ((switch_num < 1) || (switch_num > CAS_NUM_SWITCHES)) {
+        nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_ValueError,
 			"not a valid SWITCH number: %d", switch_num));
-	}
+	} else {
+        litex_switch_obj_t* sw = m_new_obj(litex_switch_obj_t);
+        sw->base.type = type_in;
+        sw->num = switch_num;
+        return MP_OBJ_FROM_PTR(sw);
+    }
 }
 
 void litex_switch_print(const mp_print_t *print, mp_obj_t self_in, mp_print_kind_t kind) {
-	litex_switch_obj_t *self = self_in;
-	mp_printf(print, "SWITCH(%u)", self->num);
+	litex_switch_obj_t *self = MP_OBJ_TO_PTR(self_in);
+	mp_printf(print, "Switch(%u)", self->num);
 }
 
 STATIC mp_obj_t litex_switch_read(mp_obj_t self_in) {
-	litex_switch_obj_t *sw = self_in;
+	litex_switch_obj_t *sw = MP_OBJ_TO_PTR(self_in);
 	bool state = cas_switches_in_read() & (1 << (sw->num - 1));
 
 	return mp_obj_new_bool(state);
@@ -59,8 +51,11 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_1(litex_switch_read_obj, litex_switch_read);
 
 
 
-STATIC const mp_map_elem_t litex_switches_locals_dict_table[] = {
-	{ MP_OBJ_NEW_QSTR(MP_QSTR_read), (mp_obj_t)&litex_switch_read_obj },
+STATIC const mp_rom_map_elem_t litex_switches_locals_dict_table[] = {
+	{ MP_OBJ_NEW_QSTR(MP_QSTR_read), MP_ROM_PTR(&litex_switch_read_obj) },
+
+    //class constants
+    { MP_OBJ_NEW_QSTR(MP_QSTR_NUM_SWITCHES), MP_OBJ_NEW_SMALL_INT(CAS_NUM_SWITCHES) },
 };
 STATIC MP_DEFINE_CONST_DICT(litex_switches_locals_dict, litex_switches_locals_dict_table);
 
--- litex/modlitex.c
+++ litex/modlitex.c
@@ -2,12 +2,14 @@
 
 extern const mp_obj_type_t litex_led_type;
 extern const mp_obj_type_t litex_switch_type;
+extern const mp_obj_type_t litex_button_type;
 
 STATIC const mp_rom_map_elem_t litex_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_litex) },
 
-    { MP_ROM_QSTR(MP_QSTR_LED), MP_ROM_PTR(&litex_led_type) },
-    { MP_ROM_QSTR(MP_QSTR_SWITCH), MP_ROM_PTR(&litex_switch_type) },
+    { MP_ROM_QSTR(MP_QSTR_Led), MP_ROM_PTR(&litex_led_type) },
+    { MP_ROM_QSTR(MP_QSTR_Switch), MP_ROM_PTR(&litex_switch_type) },
+    { MP_ROM_QSTR(MP_QSTR_Button), MP_ROM_PTR(&litex_button_type) },
 };
 
 STATIC MP_DEFINE_CONST_DICT(litex_module_globals, litex_module_globals_table);
